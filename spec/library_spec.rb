ead60c0ef164b7d60114988c3500e1c27dc99923
Hamish Arro <Hamisharro@icloud.com>
2021-01-14 11:27:03 +0000

    add list books test to make sure its outputing stored books

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -2,13 +2,18 @@ require 'library'
 
 describe Library do
 
-  it "should add a book to library" do
+  it "add a book to library" do
     expect(subject).to respond_to(:add_book).with(1).argument
   end
 
-  it "should store book" do
+  it "store book" do
     subject.add_book("Harry Potter and the Philosopher's Stone")
     expect(subject.book_list).to include "Harry Potter and the Philosopher's Stone"
+  end
+
+  it "list books" do
+    subject.add_book("Harry Potter and the Philosopher's Stone")
+    expect{ subject.list_books }.to output { "Harry Potter and the Philosopher's Stone" }.to_stdout
   end
 
 end
